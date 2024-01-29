c11b2cbe349676e646c596c173f396edfb546714
Steven Rodas <contact@stevenrodas.com>
2024-01-28 21:19:54 -0500

    HTML To Json

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+const fs = require('fs'); // fs = filesystem for web server where code is running
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8')); // read the trips.json file 
+
+
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', { title: pageTitle });
+    res.render('travel', { title: pageTitle, trips });
   }
   
   // this is used to render the index page
